7ad72351c8b91950cf17a59cfcffcda548d08b46
Justin Reich <justinreich.dev@gmail.com>
2016-08-17 09:47:10 -0500

    Improved styling

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -38,7 +38,7 @@
 
             // 
             $location.html("");
-            $location.append(data.city.name + " " + "<span class='coordinants'>Latitude: " + data.city.coord.lat + " Longitude: " + data.city.coord.lon +"</span>");
+            $location.append(data.city.name + " " + "<span class='coordinants'>Latitude: " + data.city.coord.lat + "</span><span class='coordinants'>Longitude: " + data.city.coord.lon +"</span>");
 
             // Create a forEach loop to iterate over each day of forcast
             data.list.forEach(function(element, index, array) {
@@ -50,17 +50,17 @@
             // Extract the needed information for each day and display to each panel
                 var temprature = parseInt(element.temp.max) + "&deg;" + "/" + parseInt(element.temp.min) + "&deg;";
                 var iconUrl = "http://openweathermap.org/img/w/" + element.weather[0].icon +".png";
-                var conditions = element.weather[0].main + ": " + element.weather[0].description;
-                var humidity = "Humidity" + ": " +  element.humidity + "%";
-                var windSpeed = "Wind" + ": " + parseInt(element.speed) + " mph";
-                var pressure = "pressure" + ": " + parseInt(element.pressure) + " mb";
+                var conditions = "<strong>" + element.weather[0].main + "</strong>" + ": " + element.weather[0].description;
+                var humidity = "<strong>Humidity</strong>" + ": " +  element.humidity + "%";
+                var windSpeed = "<strong>Wind</strong>" + ": " + parseInt(element.speed) + " mph";
+                var pressure = "<strong>Pressure</strong>" + ": " + parseInt(element.pressure) + " mb";
                 content += "<h3>" + "Day " + (index+1) + "</h3>";
                 content += "<h3>" + temprature + "</h3>";
                 content += "<img src=" + iconUrl + ">";
                 content += "<p>" + conditions + "</p>";
                 content += "<p>" + humidity + "</p>";
                 content += "<p>" + windSpeed + "</p>";
-                content += "<p>" + pressure + "</p>";
+                content += "<p class='last'>" + pressure + "</p>";
 
                 // Append each bit of content to the proper td panel
                 currentPanel.append(content);
@@ -86,24 +86,77 @@ function buttonFunctionality () {
         var $latitude = $('#latitude').val();
         var $longitude = $('#longitude').val();
 
-
-        alert("valid input" + $latitude + "/" + $longitude);
-
-
         findWeather($latitude, $longitude);
     }
 
 }
 
 
+
+// ================================== Google Maps ================================== //
+
+    
+
+    // Set our map options
+    var mapOptions = {
+        // Set the zoom level
+        zoom: 4,
+
+        // This sets the center of the map at our location
+        center: {
+            lat:  29.428070695992464,
+            lng:  -98.4891371968231
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+
+    var i = 0;
+
+    console.log(mapOptions);
+
+    // Render the map
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+
+    // Add marker
+
+    var currentPosition = {lat: 29.428070695992464, lng: -98.4891371968231 }
+
+    var marker = new google.maps.Marker({
+        position: currentPosition,
+        map: map,
+        draggable:true,
+        title:"Drag me!"
+    });
+
+
+ 
+
+    google.maps.event.addListener(marker, 'dragend', function (event) {
+        document.getElementById("latitude").value = this.getPosition().lat();
+        document.getElementById("longitude").value = this.getPosition().lng();
+        buttonFunctionality();
+    });
+
+ 
+
+
 // ================================== Procedure ================================== //
 
-
+    // Arm button
     $('#updateButton').click(function() {
         console.log('Button click worked')
         buttonFunctionality();
     });
+
+    $('input').hide();
+    $('button').hide();
+
+
+    buttonFunctionality();
     
-    // findWeather();
+    
+
 
 // });
