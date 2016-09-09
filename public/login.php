9511a162f1b92bc3bcef06e1324cd95fa956caa1
Justin Reich <justinreich.dev@gmail.com>
2016-09-09 14:46:37 -0500

    Two new exercises handling a user login

---
+++ public/login.php
@@ -0,0 +1,82 @@
+<!-- codeup.dev/login.php -->
+
+<?php
+
+function pageController() 
+{
+    $data = [];
+    $data['message'] = '';
+    if (!empty($_POST)) {
+        if ($_POST['username'] == 'guest' && $_POST['password'] == 'password') {
+            header("Location: /authorized.php");
+            die;
+        } else {
+            $data['message'] = 'Invalid login'; // improve UI with bootstrap alert
+        }
+    }  
+    return $data;
+} 
+extract(pageController());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login Page</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="===PATH HERE===">
+    <style>
+        main {
+            margin-top: 50px;
+        }
+        h2 {
+            color: darkred;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <main class="container">
+    <form method="POST">
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input id="username" class="form-control" type="text" name="username" placeholder="Enter User Name" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input id="password" class="form-control" type="password" name="password" placeholder="Enter Password" required>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    <h2><?= $message; ?></h2>
+
+    </main>
+    
+    <!-- jQuery Version 1.11.1 -->
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- Custom JS -->
+
+    <script src="===PATH HERE==="></script>
+
+</body>
+
+</html>
