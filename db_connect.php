007231466f521a6586f78f36dee7c0f7c5864e11
Justin Reich <justinreich.dev@gmail.com>
2016-09-22 14:31:34 -0500

    Connecting MySQL to PHP exercise

---
+++ db_connect.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'getmein');
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
