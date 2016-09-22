094371f4a781ed2caa6626d1b31d055f366faee4
Justin Reich <justinreich.dev@gmail.com>
2016-09-22 16:34:15 -0500

    Completed step 3

--- park_migration.php
+++ park_migration.php
@@ -5,3 +5,17 @@ DEFINE ('DB_USER', 'parks_user');
 DEFINE ('DB_PASS', 'parkme');
 
 require 'db_connect.php';
+
+$dbc->exec('DROP TABLE IF EXISTS national_parks;');
+
+$dbc->exec(
+    'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    date_established DATE NOT NULL,
+    area_in_acres DOUBLE NOT NULL,
+    PRIMARY KEY (id)
+    );'
+);
+
