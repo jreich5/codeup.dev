369013e85bae19657fef17b9286f34e1daf40cda
Justin Reich <justinreich.dev@gmail.com>
2016-09-22 16:18:44 -0500

    Completed steps for Query DB lesson up to beginning of step 3

---
+++ park_migration.php
@@ -0,0 +1,7 @@
+<?php 
+DEFINE ('DB_HOST', '127.0.0.1');
+DEFINE ('DB_NAME', 'parks_db');
+DEFINE ('DB_USER', 'parks_user');
+DEFINE ('DB_PASS', 'parkme');
+
+require 'db_connect.php';
---
+++ pdo_test.php
@@ -0,0 +1,8 @@
+<?php 
+
+DEFINE ('DB_HOST', '127.0.0.1');
+DEFINE ('DB_NAME', 'employees');
+DEFINE ('DB_USER', 'codeup');
+DEFINE ('DB_PASS', 'getmein');
+
+require 'db_connect.php';
