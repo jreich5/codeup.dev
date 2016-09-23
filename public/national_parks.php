eb936f6a254e3e358873be5ac6895eb2d861d4b1
Justin Reich <justinreich.dev@gmail.com>
2016-09-23 15:49:31 -0500

    Refactored into page controller and improved styling

--- public/national_parks.php
+++ public/national_parks.php
@@ -8,29 +8,27 @@ DEFINE ('DB_USER', 'parks_user');
 DEFINE ('DB_PASS', 'parkme');
 
 require_once("../db_connect.php");
-
-$count = (isset($_GET['count'])) ? $_GET['count'] : 0;
-$parks = getInfo($dbc, $count);
-$query = '?count=';
    
-function getInfo($dbc, $count) 
+function getInfo($dbc, $page, $limit) 
 {
-    $stm = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET " . $count . ";");
-    // $stm = $dbc->query("SELECT * FROM national_parks LIMIT 4, " . $count . ";");
+
+    $offset = ($page > 1) ? $page * $limit : 0;
+    $stm = $dbc->query("SELECT * FROM national_parks LIMIT " . $limit . " OFFSET " . $offset . ";");
     $rows = $stm->fetchALL(PDO::FETCH_ASSOC);
     return $rows;
 }
 
-// function pageController()
-// {
-//     $data = [];
-//     $data['parks'] = getInfo($dbc);
-//     $data['count'] = (isset($_GET['count'])) ? $_GET['count'] : 1;
-//     $data['query'] = '?count=';
-//     return $data;
-// }
+function pageController($dbc)
+{
+    $data = [];
+    $data['page'] = (isset($_GET['page'])) ? $_GET['page'] : 1;
+    $data['limit'] = 4;
+    $data['parks'] = getInfo($dbc, $data['page'], $data['limit']);
+    $data['query'] = '?page=';
+    return $data;
+}
 
-// extract(pageController());
+extract(pageController($dbc));
 
 
 ?>
@@ -40,7 +38,6 @@ function getInfo($dbc, $count)
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -51,19 +48,11 @@ function getInfo($dbc, $count)
 
     <!-- Bootstrap Core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
     <!-- Custom CSS -->
     <link rel="stylesheet" type="text/css" href="===PATH HERE===">
-
-    <style type="text/css">
-
-
-    </style>
-
 </head>
 
 <body>
-
     <main class="container"> 
         <?php  
             echo '<table class="table table-bordered"><tr><th>Name</th><th>Location</th><th>Date Established</th><th>Area</th></tr>';
@@ -72,34 +61,32 @@ function getInfo($dbc, $count)
             }
             echo '</table>';
         ?>
-        <a href="<?= $query . ($count = 0); ?>"><button>1</button></a>
-        <a href="<?= $query . ($count = 4); ?>"><button>2</button></a>
-        <a href="<?= $query . ($count = 8); ?>"><button>4</button></a>
-        <a href="<?= $query . ($count = 12); ?>"><button>5</button></a>
-        <a href="<?= $query . ($count = 16); ?>"><button>6</button></a>
-        <a href="<?= $query . ($count = 20); ?>"><button>7</button></a>
-        <a href="<?= $query . ($count = 24); ?>"><button>8</button></a>
-        <a href="<?= $query . ($count = 28); ?>"><button>9</button></a>
-        <a href="<?= $query . ($count = 32); ?>"><button>10</button></a>
-        <a href="<?= $query . ($count = 36); ?>"><button>11</button></a>
-        <a href="<?= $query . ($count = 40); ?>"><button>12</button></a>
-        <a href="<?= $query . ($count = 44); ?>"><button>13</button></a>
-        <a href="<?= $query . ($count = 48); ?>"><button>14</button></a>
-        <a href="<?= $query . ($count = 52); ?>"><button>15</button></a>
-        <a href="<?= $query . ($count = 56); ?>"><button>16</button></a>
-    
+        <!-- <?php 
+            for($i = 0; $i < ;)
+        ?> -->
+        <div class="btn-group" role="group" aria-label="...">
+            <a href="<?= $query . ($page = 1); ?>"><button class="btn btn-default">1</button></a>
+            <a href="<?= $query . ($page = 2); ?>"><button class="btn btn-default">2</button></a>
+            <a href="<?= $query . ($page = 3); ?>"><button class="btn btn-default">3</button></a>
+            <a href="<?= $query . ($page = 4); ?>"><button class="btn btn-default">4</button></a>
+            <a href="<?= $query . ($page = 5); ?>"><button class="btn btn-default">5</button></a>
+            <a href="<?= $query . ($page = 6); ?>"><button class="btn btn-default">6</button></a>
+            <a href="<?= $query . ($page = 7); ?>"><button class="btn btn-default">7</button></a>
+            <a href="<?= $query . ($page = 8); ?>"><button class="btn btn-default">8</button></a>
+            <a href="<?= $query . ($page = 9); ?>"><button class="btn btn-default">9</button></a>
+            <a href="<?= $query . ($page = 10); ?>"><button class="btn btn-default">10</button></a>
+            <a href="<?= $query . ($page = 11); ?>"><button class="btn btn-default">11</button></a>
+            <a href="<?= $query . ($page = 12); ?>"><button class="btn btn-default">12</button></a>
+            <a href="<?= $query . ($page = 13); ?>"><button class="btn btn-default">13</button></a>
+            <a href="<?= $query . ($page = 14); ?>"><button class="btn btn-default">14</button></a>
+        </div>
+
     </main>
     
     <!-- jQuery Version 1.11.1 -->
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <!-- Custom JS -->
-
-    <script src="===PATH HERE==="></script>
-
 </body>
-
 </html>
